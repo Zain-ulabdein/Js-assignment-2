15b966fb647448b7d8612366928a32d70b087a85
Zain <syed.muhammad.zain.akbber@gmail.com>
2024-03-22 03:41:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let num1 = parseInt(prompt("first number"));
+console.log("this is first number " + num1);
+
+let num2 = parseInt(prompt("second number"));
+console.log("this is second number " + num2);
+
+let num3 = parseInt(prompt("third number"));
+console.log("this is third number " + num3);
+
+console.log("sum of three number", num1 + num2 + num3);
